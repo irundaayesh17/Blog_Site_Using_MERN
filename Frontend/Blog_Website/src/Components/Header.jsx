7559e8dae58d6834d23d56a8c7a4696e856d43b8
Ayesh <ayeshirunda1234@gmail.com>
2024-08-16 23:26:29 +0530

    first commit

---
+++ Frontend/Blog_Website/src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='flex'>
+      <div className='w-72 h-screen bg-blue-950'>
+        <h1 className='text-white text-2xl font-semibold mt-8 ml-8'>Knowledge</h1>
+      </div>
+    </div>
+  )
+}
