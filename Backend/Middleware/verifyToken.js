5b0593402c22b58f9a97adacbc7a364d6d877301
irundaayesh17 <ayeshirunda1234@gmail.com>
2024-09-24 00:35:30 +0530

    Update verifyToken middleware

--- Backend/Middleware/verifyToken.js
+++ Backend/Middleware/verifyToken.js
@@ -1,17 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export const verifytoken = (req, res, next) => {
-	const token = req.cookies.token;
-	if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
-	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
 
-		if (!decoded) return res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-		req.userId = decoded.id;
-		next();
-	} catch (error) {
-		console.log("Error in verifyToken ", error);
-		return res.status(500).json({ success: false, message: "Server error" });
-	}
+    if (!decoded) return res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
+
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ success: false, message: "Unauthorized - token expired" });
+    }
+    console.log("Error in verifyToken ", error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
 };
